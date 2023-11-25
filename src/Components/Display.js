4674651e52131f3b045724308855e801e2c604ab
Pranav Raju <pranavraju2020@gmail.com>
2023-11-25 23:22:15 +0530

    search done

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -3,20 +3,43 @@ import { useBookContext } from "../context/BookContext";
 
 const Display = () => {
   const { title, disc, bookImg } = useBookContext();
+  // if (!bookImg[0].smallThumbnail == undefined) {
+  //   return;
+  // }
 
   console.log("displaying display");
   console.log("Title:", title);
   console.log("Description:", disc);
-  console.log("Image:", bookImg);
+  // console.log("Image:", bookImg[0].smallThumbnail);
+  // console.log(bookImg[0].smallThumbnail);
 
   return (
-    <div>
-      {/* <h2>Title: {title}</h2>
-      <p>Description: {disc}</p>
-      <img src={bookImg} alt="Book Cover" /> */}
-      Display
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(5,1fr)",
+      }}
+    >
+      {title &&
+        title.map((value, index) => (
+          <div key={index}>
+            {bookImg[index] && bookImg[index].smallThumbnail ? (
+              <img src={bookImg[index].smallThumbnail} />
+            ) : (
+              <p>No Image Available</p>
+            )}
+            <p style={{ color: "red", fontWeight: "bold" }}>{value}</p>
+            <p>{disc[index]}</p>
+          </div>
+        ))}
     </div>
   );
 };
 
 export default Display;
+
+// {/* <div className="row"> */}
+// {title.map((item) => (
+//   <div>{item}</div>
+// ))}
+// {/* </div> */}
